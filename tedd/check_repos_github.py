2405a0099a26a288c6d555f6aff13ef9069a65ba
Fabio Di Lauro <fabio.dilauro@gmail.com>
2023-09-05 10:16:17 +0200

    check repos with selenium

--- tedd/check_repos_github.py
+++ tedd/check_repos_github.py
@@ -1,13 +1,19 @@
 from github import Github
 import sys, time
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 # Crea un'istanza Github senza autenticazione
-g = Github('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')
+#
 
+token = os.environ.get('GITHUB_TOKEN')
+g = Github(token)
 # Cerca i repository
-repositories = g.search_repositories(query='symbol:find_element_by_ language:java')
+repositories = g.search_repositories(query='language:java content:selenium')
 
 # Stampa l'URL di ciascun repository nei risultati
 for repo in repositories:
     print(repo.html_url)
-    sys.exit()
+    input()
+
+    # sys.exit()
