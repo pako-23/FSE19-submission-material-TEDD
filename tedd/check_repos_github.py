c9df5ec151e5e8fb81381a9dab76f45f06d987dd
Fabio Di Lauro <fabio.dilauro@gmail.com>
2023-09-05 11:02:41 +0200

    check repos

--- tedd/check_repos_github.py
+++ tedd/check_repos_github.py
@@ -3,17 +3,24 @@ import sys, time
 import os
 from dotenv import load_dotenv
 load_dotenv()
-# Crea un'istanza Github senza autenticazione
-#
 
 token = os.environ.get('GITHUB_TOKEN')
 g = Github(token)
 # Cerca i repository
 repositories = g.search_repositories(query='language:java content:selenium')
 
-# Stampa l'URL di ciascun repository nei risultati
-for repo in repositories:
-    print(repo.html_url)
-    input()
+if not os.path.exists('repos'):
+    os.makedirs('repos')
 
-    # sys.exit()
+for repo in repositories:
+    owner_name = repo.full_name.split('/')[0]  # Extract owner name from full name
+    
+    # Create directory for the owner if it doesn't exist
+    target_dir = os.path.join('repos', owner_name)
+    if not os.path.exists(target_dir):
+        os.makedirs(target_dir)
+    else:
+        continue  # Skip if the directory already exists
+    
+    os.system(f"git clone {repo.clone_url} {target_dir}/{repo.name}")
+    time.sleep(1)  # Sleep for 1 second to avoid abuse detection
