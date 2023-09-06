29234db5cd980d532804035731d35ab6986c96ec
Fabio Di Lauro <fabio.dilauro@gmail.com>
2023-09-06 09:15:53 +0200

    build dockerfiles

--- tedd/build_dockerfiles.py
+++ tedd/build_dockerfiles.py
@@ -3,19 +3,35 @@ import subprocess
 
 BASE_DIR = 'repos'
 
-def build_docker_image(repo_path):
+def stop_and_remove_container(image_name):
     try:
-        result = subprocess.run(['docker', 'build', '-t', 'test_image', repo_path], capture_output=True, check=True, text=True)
-        print(f"[SUCCESS] Immagine Docker costruita con successo per {repo_path}")
-        return True
+        # Stop container
+        subprocess.run(['docker', 'stop', image_name], check=True)
+    except subprocess.CalledProcessError:
+        pass
+
+    try:
+        # Remove container
+        subprocess.run(['docker', 'rm', image_name], check=True)
+    except subprocess.CalledProcessError:
+        pass
+
+def build_docker_image(repo_path):
+    image_name = 'test_image'
+    
+    # Stop and remove any containers from previous builds
+    stop_and_remove_container(image_name)
+    
+    try:
+        result = subprocess.run(['docker', 'build', '-t', image_name, repo_path], capture_output=True, check=True, text=True)
+        print(f"[SUCCESS] Docker image built successfully for {repo_path}")
     except subprocess.CalledProcessError as e:
-        print(f"[ERROR] Errore durante la costruzione dell'immagine Docker per {repo_path}: {e.stderr}")
-        return False
+        print(f"[ERROR] Error building Docker image for {repo_path}: {e.stderr}")
 
 def main():
     for root, dirs, files in os.walk(BASE_DIR):
         if 'Dockerfile' in files:
-            print(f"Verifica Dockerfile in: {root}")
+            print(f"Verifying Dockerfile in: {root}")
             build_docker_image(root)
 
 if __name__ == "__main__":
