a69cbe2eacaac5572903474165f136a403922721
Fabio Di Lauro <fabio.dilauro@gmail.com>
2023-09-11 10:02:11 +0200

    run algo/ts 10 times

---
+++ tedd/run-10-times.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+declare -a algorithms=("ex-linear" "big-table" "pradet")
+declare -a testsuites=("addressbook" "claroline" "mantisbt" "mrb" "ppma")
+
+for algo in "${algorithms[@]}"; do
+    for ts in "${testsuites[@]}"; do
+        for i in {1..10}; do
+            TIMESTAMP=$(date "+%Y%m%d_%H%M%S")
+            nohup ./run-app.sh -a ${algo} -t ppm > "results/${algo}_${ts}_output_${TIMESTAMP}.log" 2>&1 &
+        done
+    done
+done
